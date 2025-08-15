01b12d8e771029fac8851f6d1d73cacdb95b7847
UTDRON <pokharelprateik1993@hotmail.com>
2025-08-14 23:03:51 -0400

    iterative bin_expo

---
+++ binary_exponentiation/iterative.py
@@ -0,0 +1,18 @@
+def power(a, b):
+    result = 1
+
+    while b:
+        if b % 2 == 1:
+            result = result * a
+        
+        a = a * a
+        b = b // 2
+
+    return result
+
+assert power(5,0) == 5 ** 0, f"should be {1 ** 0}"
+assert power(2,6) == 2 ** 6, f"should be {2 ** 6}"
+assert power(2,5) == 2 ** 5, f"should be {2 ** 5}"
+assert power(2,525) == 2 ** 525, f"should be {2 ** 525}"
+assert power(2,-1) == 2 ** -1, f"should be {2 ** -1}"
+    
--- binary_exponentiation/recursive.py
+++ binary_exponentiation/recursive.py
@@ -3,9 +3,11 @@ def power(a, b):
         return 1
     
     if b % 2 == 0:
-        result = power(a, b//2) * power(a, b//2)
+        temp = power(a, b//2)
+        result = temp * temp
     else:
-        result = power(a, (b-1)//2) * power(a, (b-1)//2) * a
+        temp = power(a, (b-1)//2)
+        result = temp * temp * a
 
     return result
     
@@ -14,4 +16,4 @@ assert power(5,0) == 5 ** 0, f"should be {1 ** 0}"
 assert power(2,6) == 2 ** 6, f"should be {2 ** 6}"
 assert power(2,5) == 2 ** 5, f"should be {2 ** 5}"
 assert power(2,525) == 2 ** 525, f"should be {2 ** 525}"
-assert power(2,-1) == 2 ** -1, f"should be {2 ** -1}"
+# assert power(2,-1) == 2 ** -1, f"should be {2 ** -1}"
